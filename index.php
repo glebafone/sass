72e0a7bfef489c4dbba97d6313622e7e89312897
gleb <glebafoned@gmail.com>
2023-06-06 10:11:40 +0300

    theme

---
+++ index.php
@@ -0,0 +1,445 @@
+<!doctype html>
+<html lang="en">
+  <head>
+ 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello, world!</title>
+	<link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+	<header>
+		<a href="#"></a>
+		<img src="images/logo.svg" alt="htmlbook.ru" class="logo" width="184" height="66">
+		<div class="menu__btn">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<div class="menu__burger">
+					<h2 class="title">Теги HTML</h2>
+					<div class="leftsidebar__search">
+					<input type="text" name="q" autocomplete="off"placeholder="Тег HTML или св-во CSS">
+					</div>
+					<div class="block__content">
+						<ul>
+						<li>
+							<a href="#">Все теги</a>
+						</li>	
+						<li>
+							<a href="#">Валидация тегов</a>
+						</li>
+						<li>
+							<a href="#">"!-- --"</a>
+						</li>
+						<li>
+							<a href="#">!DOCTYPE</a>
+						</li>	
+						<li>
+							<a href="#">"a"</a>
+						</li>
+						<li>
+							<a href="#">"abbr"</a>
+						</li>
+						<li>
+							<a href="#">"address</a>
+						</li>
+						<li>
+							<a href="#">"applet"</a>
+						</li>
+						<li>
+							<a href="#">"area"</a>
+						</li>
+						<li>
+							<a href="#">"article"</a>
+						</li>
+						<li>
+							<a href="#">"aside"</a>
+						</li>
+						<li>
+							<a href="#">"audio"</a>
+						</li>
+						<li>
+							<a href="#">"b"</a>
+						</li>
+						<li>
+							<a href="#">"base"</a>
+						</li>
+						<li>
+							<a href="#">"basefont"</a>
+						</li>
+						<li>
+							<a href="#">"bdi"</a>
+						</li>
+						<li>
+							<a href="#">"bdo"</a>
+						</li>
+						</ul>	
+					</div>
+					<h2 class="title">Справочник CSS</h2>
+					<div class="block__content">
+						<ul>
+						<li>
+							<a href="#">Как пользоваться справочником</a>
+						</li>
+						<li>
+							<a href="#">!important</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-bottom-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-left-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-right-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-top-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-linear-gradient</a>
+						</li>
+						<li>
+							<a href="#">-moz-orient</a>
+						</li>
+						<li>
+							<a href="#">-moz-radial-gradient</a>
+						</li>
+						<li>
+							<a href="#">-moz-user-select</a>
+						</li>
+						<li>
+							<a href="#">-ms-interpolation-mode</a>
+						</li>
+						<li>
+							<a href="#">-ms-radial-gradient</a>
+						</li>						
+						</ul>	
+					</div>
+					<h2 class="title">Самоучители</h2>
+					<div class="block__content">
+					<ul>
+					<li>
+						<a href="#">Самоучитель HTML4</a>
+					</li>	
+					<li>
+						<a href="#">Погружение в HTML5</a>
+					</li>	
+					<li>
+						<a href="#">Самоучитель HTML5</a>
+					</li>	
+					<li>
+						<a href="#">Самоучитель CSS 2.1 </a>
+					</li>
+					<li>
+						<a href="#">CSS3 на примерах</a>
+					</li>
+					<li>
+						<a href="#">Самоучитель CSS 2.1 </a>
+					</li>	
+					</ul>	
+					</div>
+					<h2 class="title">Популярные рецепты</h2>
+					<div class="block__content">
+					<ul>
+					<li>
+						<a href="#">Как добавить картинку на веб-страницу?</a>
+					</li>	
+					<li>
+						<a href="#">Как добавить иконку сайта в адресную строку браузера?</a>
+					</li>	
+					<li>
+						<a href="#">Как добавить фоновый рисунок на веб-страницу?</a>
+					</li>	
+					<li>
+						<a href="#">Как сделать обтекание картинки текстом?</a>
+					</li>
+					<li>
+						<a href="#">Как растянуть фон на всю ширину окна?</a>
+					</li>
+					<li>
+						<a href="#">Как выровнять фотографию по центру веб-страницы?</a>
+					</li>	
+					</ul>	
+					</div>
+		</div>
+		</div>
+		</div>
+		<div class="search">
+				<input type="text" name="as_q"  autocomplete="off" placeholder="Поиск по сайту">
+				<input type="submit" value="Найти">
+			</div>
+		<nav class="topmenu">
+			<ul class="topmenu__list"> 
+				<li>
+					<a href="#main">
+					<i class="icon icon-main"></i>
+					<br>
+						Основное
+					</a>
+				</li>
+				<li>
+					<a href="#html">
+					<i class="icon icon-html"></i>
+					<br>
+						HTML
+					</a>
+				</li>
+				<li>				
+					<a href="#css">
+					<i class="icon icon-css"></i>
+					<br>
+						CSS
+					</a>
+				</li>
+				<li>			
+					<a href="#site">
+					<i class="icon icon-site"></i>
+					<br>
+						Сайт
+					</a>
+				</li>				
+			</ul>
+			<ul class="menu">
+				<li>
+					<a href="#">
+						Статьи
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						Блог
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						Практикум
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						Форум
+					</a>
+				</li>
+			</ul>	
+		</nav>
+	</header>
+			<div class="search">
+				<input type="text" name="as_q"  autocomplete="off" placeholder="Поиск по сайту">
+				<input type="submit" value="Найти">
+			</div>
+			<aside class="leftsidebar">
+				<div class="leftsidebar__search">
+					<input type="text" name="q" autocomplete="off"placeholder="Тег HTML или св-во CSS">
+				</div>		
+				<div class="block">
+					<h2>Теги HTML</h2>
+					<div class="block__content">
+						<ul>
+						<li>
+							<a href="#">Все теги</a>
+						</li>	
+						<li>
+							<a href="#">Валидация тегов</a>
+						</li>
+						<li>
+							<a href="#">"!-- --"</a>
+						</li>
+						<li>
+							<a href="#">!DOCTYPE</a>
+						</li>	
+						<li>
+							<a href="#">"a"</a>
+						</li>
+						<li>
+							<a href="#">"abbr"</a>
+						</li>
+						<li>
+							<a href="#">"address</a>
+						</li>
+						<li>
+							<a href="#">"applet"</a>
+						</li>
+						<li>
+							<a href="#">"area"</a>
+						</li>
+						<li>
+							<a href="#">"article"</a>
+						</li>
+						<li>
+							<a href="#">"aside"</a>
+						</li>
+						<li>
+							<a href="#">"audio"</a>
+						</li>
+						<li>
+							<a href="#">"b"</a>
+						</li>
+						<li>
+							<a href="#">"base"</a>
+						</li>
+						<li>
+							<a href="#">"basefont"</a>
+						</li>
+						<li>
+							<a href="#">"bdi"</a>
+						</li>
+						<li>
+							<a href="#">"bdo"</a>
+						</li>
+						</ul>	
+					</div>
+				</div>
+				<div class="block">
+					<h2>Справочник CSS</h2>
+					<div class="block__content">
+						<ul>
+						<li>
+							<a href="#">Как пользоваться справочником</a>
+						</li>
+						<li>
+							<a href="#">!important</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-bottom-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-left-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-right-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-border-top-colors</a>
+						</li>
+						<li>
+							<a href="#">-moz-linear-gradient</a>
+						</li>
+						<li>
+							<a href="#">-moz-orient</a>
+						</li>
+						<li>
+							<a href="#">-moz-radial-gradient</a>
+						</li>
+						<li>
+							<a href="#">-moz-user-select</a>
+						</li>
+						<li>
+							<a href="#">-ms-interpolation-mode</a>
+						</li>
+						<li>
+							<a href="#">-ms-radial-gradient</a>
+						</li>						
+						</ul>	
+					</div>
+				</div>
+			</aside>
+		<div class="ad">
+			<div class="ad__block">
+				<img src="images/geek.png" width="280" height="150">
+				<p>Руководство по эксплуатации.Сделать!</p>
+				<p><button>Узнать больше</button></p>
+				<a href="#">standartno.by</a>
+			</div>
+			<div class="ad__block">
+				<img src="images/geek.png" width="280" height="150">
+				<p>Курс Java-разработчик - первый месяц = 0руб.</p>
+				<p><button>Узнать больше</button></p>
+				<a href="#">skillbox.ru</a>
+			</div>
+			<div class="ad__block">
+				<img src="images/geek.png" width="280" height="150">
+				<p>Промокод 100 рублей в Яндекс Директе.</p>
+				<p><button>Узнать больше</button></p>
+				<a href="#">yandex.by</a>
+			</div>
+			<div class="ad__block">
+				<img src="images/geek.png" width="280" height="150">
+				<p>HTML и CSS.Подробный видеокурс!</p>
+				<p><button>Узнать больше</button></p>
+				<a href="#">srv.mrusakov.by</a>
+			</div>
+			<div class="ad__block">
+				<img src="images/geek.png" width="280" height="150">
+				<p>Скидка до 25% на курс по Frontend</p>
+				<p><button>Узнать больше</button></p>
+				<a href="#">geekbrains.by</a>
+			</div>
+			<div class="ad__block">
+				<img src="images/geek.png" width="280" height="150">
+				<p>Бесплатный курс по Веб-Дизайну!</br></p>
+				<p><button>Узнать больше</button></p>
+				<a href="#">wayup.in</a>
+			</div>
+		</div>
+		<aside class="rightsidebar">
+			<div class="rightsidebar__block">
+				<h2>Самоучители</h2>
+				<div class="rightsidebar__content">
+					<ul class="rightsidebar__bll">
+					<li>
+						<a href="#">Самоучитель HTML4</a>
+					</li>	
+					<li>
+						<a href="#">Погружение в HTML5</a>
+					</li>	
+					<li>
+						<a href="#">Самоучитель HTML5</a>
+					</li>	
+					<li>
+						<a href="#">Самоучитель CSS 2.1 </a>
+					</li>
+					<li>
+						<a href="#">CSS3 на примерах</a>
+					</li>
+					<li>
+						<a href="#">Самоучитель CSS 2.1 </a>
+					</li>	
+					</ul>	
+				</div>
+			</div>
+			<div class="rightsidebar__block">
+				<h2>Популярные рецепты</h2>
+				<div class="rightsidebar__content">
+					<ul class="rightsidebar__bl">
+					<li>
+						<a href="#">Как добавить картинку на веб-страницу?</a>
+					</li>	
+					<li>
+						<a href="#">Как добавить иконку сайта в адресную строку браузера?</a>
+					</li>	
+					<li>
+						<a href="#">Как добавить фоновый рисунок на веб-страницу?</a>
+					</li>	
+					<li>
+						<a href="#">Как сделать обтекание картинки текстом?</a>
+					</li>
+					<li>
+						<a href="#">Как растянуть фон на всю ширину окна?</a>
+					</li>
+					<li>
+						<a href="#">Как выровнять фотографию по центру веб-страницы?</a>
+					</li>	
+					</ul>	
+					</div>
+				</div>
+		</aside>
+		<div class="content">
+			<div class="news">
+				<article class="post">
+					<h2>
+						<a href="#">Старт карьеры во фронтенд-разработке: что нужно знать новичку?</a>
+					</h2>
+					<div>
+						<p>Как войти в профессию веб-разработчика и насколько это сложно? Что учить и в каком направлении двигаться, чтобы получить первую работу?</p>
+						<p>6 апреля в 13:00 пройдет лайв, на котором Лёша Симоненко, директор по развитию HTML Academy и веб-разработчик с опытом более 20 лет, ответит на все вопросы о старте карьеры в IT.</p>
+						<p>Запишитесь на 2айв, чтобы не пропустить событие. Всем участникам подарим скидки на профессии HTML Academy и пришлём полезные материалы по теме.</p>
+						<p>А чтобы не пропустить мероприятие, рекомендуем подписаться на телеграм-бота.</p>
+					</div>	
+				</article>
+			</div>
+		</div>
+	<script src="main.js"></script>
+  </body>
+</html>
